66cc8ec2ee1c7a0287d4051e82409351850822a7
unknown <noedaboville@yahoo.fr>
2025-05-14 21:55:03 +0200

    update cure

--- cure.ps1
+++ cure.ps1
@@ -12,7 +12,7 @@ Set-Culture $LanguageTag
 Get-Process -Name "Narrator" -ErrorAction SilentlyContinue | Stop-Process -Force
 
 # -------------------- RÉTABLIR LE FOND D'ÉCRAN PAR DÉFAUT --------------------
-$DefaultWallpaperPath = "C:\Windows\Web\Wallpaper\Theme1\img1.jpg"  # Chemin du fond d'écran par défaut
+$DefaultWallpaperPath = "C:\Windows\Web\Wallpaper\Windows\img0.jpg"  # Chemin du fond d'écran par défaut
 Add-Type -TypeDefinition @"
 using System.Runtime.InteropServices;
 public class Wallpaper {
@@ -24,7 +24,21 @@ public class Wallpaper {
 
 # -------------------- RÉTABLIR LE CURSEUR PAR DÉFAUT --------------------
 Set-ItemProperty -Path "HKCU:\Control Panel\Cursors" -Name "Arrow" -Value "C:\Windows\Cursors\aero_arrow.cur"  # Curseur par défaut
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+# Forcer l'application immédiate
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class NativeMethods {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+# SPI_SETCURSORS = 0x0057
+# SPIF_SENDCHANGE = 0x02
+[void][NativeMethods]::SystemParametersInfo(0x57, 0, $null, 0x02)
 
 # -------------------- FIN --------------------
 Write-Host "Les paramètres ont été rétablis à leurs valeurs par défaut !"
